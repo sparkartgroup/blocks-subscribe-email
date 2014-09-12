4bc3e0f61aebc4d52f1f34621aedd0aa8502b203
Josiah Sprague <josiah.sprague@gmail.com>
2014-09-12 13:29:58 -0400

    [#MmLRZ2E2] package as UMD
    I think this works 🙈
    
    Branch: MmLRZ2E2-development

--- gulp/browserify.js
+++ gulp/browserify.js
@@ -6,29 +6,19 @@ var source       = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
   var bundler = browserify({
-    // Required watchify args
-    cache: {}, packageCache: {}, fullPaths: true,
-    // Specify the entry point of your app
-    entries: ['./demo/bundle.js'],
-    // Add file extentions to make optional in your requires
-    extensions: ['.hbs']
+    entries: ['./src/subscribe-email.js'],
+    extensions: ['.hbs'],
+    standalone: 'SubscribeEmail'
   });
 
   var bundle = function() {
-    // Log when bundling starts
     bundleLogger.start();
 
     return bundler
       .bundle()
-      // Report compile errors
       .on('error', handleErrors)
-      // Use vinyl-source-stream to make the
-      // stream gulp compatible. Specifiy the
-      // desired output filename here.
       .pipe(source('subscribe-email.js'))
-      // Specify the output destination
       .pipe(gulp.dest('./build/'))
-      // Log when bundling completes!
       .on('end', bundleLogger.end);
   };
 
