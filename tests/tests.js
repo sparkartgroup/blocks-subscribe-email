83c083309e980477edce9263e813301deb1953dc
Josiah Sprague <josiah.sprague@gmail.com>
2014-09-17 18:22:26 -0400

    [#MmLRZ2E2] Update test configuration
    Branch: MmLRZ2E2-development

--- tests/tests.js
+++ tests/tests.js
@@ -1,21 +1,19 @@
 var SubscribeEmail = require('../build/subscribe-email.js');
 var test = require('tape');
 
-document.addEventListener('DOMContentLoaded', function(){
+//Setup
+var theElement = document.createElement('form');
+document.body.appendChild(theElement);
+theElement.id = 'subscribe';
 
-  var theElement = document.createElement('form');
-  document.body.appendChild(theElement);
-  theElement.id = 'subscribe';
-
-  new SubscribeEmail({
-    element: '#subscribe',
-    service: 'universe',
-    template: true,
-    key: '2366edcf-805b-43bf-b043-9c2f527967d9'
-  });
-
+new SubscribeEmail({
+  element: '#subscribe',
+  service: 'universe',
+  template: true,
+  key: '2366edcf-805b-43bf-b043-9c2f527967d9'
 });
 
+//Tests
 test('SubscribeEmail exists and is a function', function (t) {
   var objType = typeof SubscribeEmail;
   t.equal(objType, 'function');
